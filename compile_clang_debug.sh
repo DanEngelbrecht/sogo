6c27c1cc1089d3d98675643fec009e9f7036936b
Dan Engelbrecht <dan@engelbrecht.com>
2019-01-20 11:15:52 +0100

    Added compile clang debug

--- compile_clang_debug.sh
+++ compile_clang_debug.sh
@@ -3,9 +3,10 @@
 set +e
 mkdir -p build
 
-OPT=-O3
+#OPT=-O3
+OPT="-g -O1"
 #DISASSEMBLY='-S -masm=intel'
-ASAN=""
+ASAN="-fsanitize=address -fno-omit-frame-pointer"
 CXXFLAGS="$CXXFLAGS -Wall -Weverything -pedantic -Wno-old-style-cast -Wno-sign-conversion -Wno-padded -Wno-unused-macros -Wno-c++98-compat -Wno-implicit-fallthrough"
 ARCH=-m64
 
