0324ff2d523b70541b98729c724eb1f3b4b53323
Dan Engelbrecht <dan@engelbrecht.com>
2019-01-20 11:13:33 +0100

    Simpler build on windows

--- compile_cl.bat
+++ compile_cl.bat
@@ -8,7 +8,10 @@ IF NOT EXIST build (
     mkdir build
 )
 
-cl.exe /nologo /c /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\sogo.obj src\sogo.cpp
-cl.exe /nologo /c /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\sogo_nodes.obj src\sogo_nodes.cpp
-cl.exe /nologo /c /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\xxhash.obj third-party\xxHash-1\xxhash.c
-cl.exe /nologo /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/main.cpp /Fo.\build\main.obj /link .\build\sogo.obj .\build\sogo_nodes.obj .\build\xxhash.obj /out:.\build\test.exe /pdb:.\build\test.pdb
+pushd build
+
+cl.exe /nologo /Zi /O2 /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 ..\src\sogo.cpp ..\src\sogo_utils.cpp ..\src\sogo_nodes.cpp ..\third-party\xxHash-1\xxhash.c ..\test/main.cpp /link  /out:test.exe /pdb:test.pdb
+
+popd
+
+exit /B %ERRORLEVEL%
--- compile_cl_debug.bat
+++ compile_cl_debug.bat
@@ -8,7 +8,11 @@ IF NOT EXIST build (
     mkdir build
 )
 
-cl.exe /nologo /c /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\sogo.obj src\sogo.cpp
-cl.exe /nologo /c /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\sogo_nodes.obj src\sogo_nodes.cpp
-cl.exe /nologo /c /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Fo.\build\xxhash.obj third-party\xxHash-1\xxhash.c
-cl.exe /nologo /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 /Isrc test/main.cpp /Fo.\build\main.obj /link .\build\sogo.obj .\build\sogo_nodes.obj .\build\xxhash.obj /out:.\build\test.exe /pdb:.\build\test.pdb
+pushd build
+
+cl.exe /nologo /Zi /D_CRT_SECURE_NO_WARNINGS /D_HAS_EXCEPTIONS=0 /EHsc /W4 ..\src\sogo.cpp ..\src\sogo_utils.cpp ..\src\sogo_nodes.cpp ..\third-party\xxHash-1\xxhash.c ..\test/main.cpp /link  /out:test_debug.exe /pdb:test_debug.pdb
+
+popd
+
+exit /B %ERRORLEVEL%
+
