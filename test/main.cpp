ecea54434aa195878df54eb02ab9eafe8fef009c
Dan Engelbrecht <dan@engelbrecht.com>
2019-01-27 23:42:35 +0100

    Tests uses https://github.com/DanEngelbrecht/jctest Updated bikeshed

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,5 @@
 #define JC_TEST_IMPLEMENTATION
-#include "../third-party/containers/test/jc_test.h"
+#include "../third-party/jctest/jc_test.h"
 #include "test_sogo.cpp"
 
 int main(int argc, const char** argv)
